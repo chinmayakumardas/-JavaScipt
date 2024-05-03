02948b99284b3e6f963ed9c96463b49e906e2380
chinmayakumardas <chinmayakumardas2000@gmail.com>
2024-05-03 14:10:12 +0530

    Day_19

---
+++ Day_19.js
@@ -0,0 +1,17 @@
+//write a function chack the given string starts with a specific substring.
+
+function startWith(str,substr){
+    //way 1
+//   return str.startsWith(substr)
+
+//way2
+// return substr===str.slice(0,substr.length)
+
+
+}
+
+
+
+
+console.log(startWith("chinmaya","chin"))
+console.log(startWith("chinmaya","aya"))
