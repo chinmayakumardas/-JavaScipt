d0b0083b6a7a41d2d8b36c34813c21abc319b766
chinmayakumardas <chinmayakumardas2000@gmail.com>
2024-05-03 14:22:09 +0530

    Day_20 completed

---
+++ Day_20.js
@@ -0,0 +1,17 @@
+//write a function to reverses string without using any type of  libreary func
+
+//way 1
+// function reverse(str){
+//     let rev='';
+//     for(let i=str.length-1;i>=0;i--){
+//             rev+=str[i]
+//     }
+//     return rev;
+// }
+
+
+//way 2
+
+
+console.log(reverse("chinmaya"))
+
