1c0b8f36725bad13f700311597a4742b3a60a461
chinmayakumardas <chinmayakumardas2000@gmail.com>
2024-05-08 06:19:15 +0530

    day 27 completed

---
+++ Day_27.js
@@ -0,0 +1,11 @@
+//write a function called repeatString that takes 2 parameter
+
+function repeatString(str,times){
+    return str.repeat(times);
+}
+
+
+
+console.log(
+    repeatString("abc",5)
+)
