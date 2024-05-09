80682362ef958eaf4c08beb08b32e2a7f498efe5
chinmayakumardas <chinmayakumardas2000@gmail.com>
2024-05-09 08:15:03 +0530

    day 31

--- Day_31.js
+++ Day_31.js
@@ -9,11 +9,19 @@ function simplePasswordValidator(password){
     if(password.length < 8){
         return false;
     } else {
-        if(/[a-z]/.test(password) && /[A-Z]/.test(password) && /\d/.test(password)){
+        //way1
+        if(/[A-Z]/.test(password.charAt(0))&&  /[a-z]/.test(password) && /[A-Z]/.test(password) && /\d/.test(password)){
             return true;
         } else {
             return false;
         }
+
+        //way2 but it not suitable to check some time s it fails
+        // if(password.includes("a-z") && password.includes("A-Z") && password.includes("0-9")){
+        //     return true;
+        // } else {
+        //     return false;
+        // }
     }
 }
 
@@ -22,6 +30,6 @@ function simplePasswordValidator(password){
 console.log(
     simplePasswordValidator("Chin@2024"),
     simplePasswordValidator("Chi20"),
-    simplePasswordValidator(""),
-    simplePasswordValidator("Chimaya")
+    simplePasswordValidator("Chinmayakua@22"),
+    simplePasswordValidator("chimaya@2000")
 )
